3aeaf731bbfb65d7d72c4941730724b907876baa
Hank Jackson <hjackson277@gmail.com>
2023-03-08 20:55:44 -0500

    moved video to audio stream in rtmp

--- src/server/audioStream.ts
+++ src/server/audioStream.ts
@@ -1,26 +1,26 @@
 import ytdl from "ytdl-core";
 import ffmpeg from 'fluent-ffmpeg'
 
-export function handleAudioStream(){
+export function startAudioStream(){
 
-  const ref = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+  const ref = 'https://youtu.be/ICo6pRMRpkk'
 
-  const stream = ytdl(ref
-    // , { 
-    //     filter: 'audioonly',
-    //     quality: 'highestaudio'
-    //   }
-    )
+  const stream = ytdl(ref, {
+      filter: 'audioonly',
+      quality: 'highestaudio'
+    }
+  )
 
   ffmpeg(stream)
-    .inputOptions(['-re'])
+    .inputOptions([
+      '-re'
+    ])
     .outputOption([
-    '-c:v libx264',
-    '-preset veryfast',
-    '-tune zerolatency',
-    '-c:a aac',
-    '-ar 44100',
-    '-f flv'
-  ])
-  .save('rtmp://localhost/live/main.flv')
-}
+      // '-c:v libx264',
+      // '-preset veryfast',
+      // '-tune zerolatency',
+      '-c:a aac',
+      '-ar 44100',
+    ])
+    .save('rtmp://localhost/live/main.flv')
+  }
--- src/server/main.ts
+++ src/server/main.ts
@@ -2,7 +2,7 @@ import express from "express";
 import ViteExpress from "vite-express";
 import { Server } from "socket.io"
 import ffmpeg from 'fluent-ffmpeg'
-import { handleAudioStream } from "./audioStream";
+import { startAudioStream } from "./audioStream";
 import { configMediaServer } from "./configMediaServer";
 import NodeMediaServer from 'node-media-server'
 
@@ -13,16 +13,11 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-handleAudioStream()
+startAudioStream()
 
 const RTMPconfig = configMediaServer(ffmpegPath)
 const nms = new NodeMediaServer(RTMPconfig)
 nms.run()
-
-// app.get('/stream', (_, res) => {
-//   res.set('Content-Type', 'audio/mpeg')
-//   return audioStream.pipe(res)
-// })
 
 const server = ViteExpress.listen(app, 3000, () =>
   console.log("Server is listening on port 3000...")
