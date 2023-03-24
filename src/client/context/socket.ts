8792dd9f41deb78919573e5cc59040bdd6fa229e
Hank Jackson <hjackson277@gmail.com>
2023-03-24 11:28:32 -0400

    docker compose image from remote repo

--- src/client/context/socket.ts
+++ src/client/context/socket.ts
@@ -2,9 +2,7 @@ import { io } from 'socket.io-client';
 import { createContext } from 'react';
 
 
-const URL = `${import.meta.env.VITE_API_URL}:3000`;
-
-export const socket = io(URL, {
+export const socket = io('/', {
   transports: ['websocket']
 });
 export const SocketContext = createContext({
