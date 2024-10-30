dae860a3b509d1206fd9151dd0e618cf9ab075a0
Hank Jackson <hjackson277@gmail.com>
2024-10-29 22:47:56 -0400

    dl script + begin moving stream to s3

---
+++ src/server/livestream/RevalidateTags.ts
@@ -0,0 +1,30 @@
+import dotenv from 'dotenv';
+import { initDB } from "../db/initDB";
+
+dotenv.config();
+
+async function removeS3(): Promise<void> {
+  try {
+    const db = await initDB();
+    if (db){
+      console.log('connected to mongoDb: gt_data');
+    } else {
+      throw new Error('Could not connect to mongoDb: gt_data');
+    };
+
+    const posts = db.collection('gt_posts');
+
+    await posts.updateMany(
+      {link_source: "youtube"}, 
+      { $unset: {temp_invalid: ""}}
+    );
+    console.log("cleared temp invalid flag");
+
+    console.log("done!")
+  } catch(e) {
+    console.error("Could not complete uploads: ", e);
+  }
+}
+
+removeS3();
+
